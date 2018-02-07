24fb524b54e4dedff9f34ae807cabd1119b6747f
harold <dadapapa@gmail.com>
2018-02-07 17:30:17 +0000

    Open issue #1
    Added stocal.test.bugs.Issue1.
    bugs are excluded from the pre-commit hook, meaning their test cases
    can fail when commiting to develop and master, but they show up
    using `python setup.py test`. Fixed issue tests should be included in the
    regular test suite.

---
+++ stocal/tests/bugs.py
@@ -0,0 +1,16 @@
+import unittest
+import stocal
+
+class Issue1(unittest.TestCase):
+    """Issue 1 [OPEN]: https://github.com/harfel/stocal/issues/1
+
+    Opened 07-02-2018
+    """
+    def test_text_occurrence_delayed(self):
+        """assert that repeated events do not fire before first occurrence"""
+        event = stocal.Event(['a'], ['z'], 1, 1)
+        self.assertEqual(event.next_occurrence(0), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
