harfel/stocal

commit 9e1778c91236383acc21202da1c143a0de20575c

Provide TrajecorySampler.transitions
The transitions property has to return a list of all active transitions,
properly accounting for their multiplicities.

stocal/algorithms.py
@@ -37,6 +37,7 @@ their multiplicity.
 """
 
 import abc
+import warnings
 from pqdict import pqdict
 
 from ._utils import with_metaclass
@@ -102,7 +103,7 @@ class MultiDict(object):
         increased by one. Otherwise, it is stored with multiplicity
         one."""
         if key not in self._dict:
-            # insert transition with propensity and multiplicity 1
+            # insert transition with propensity and multiplicity one
             self._dict[key] = [value, 1]
         else:
             # increase multiplicity count
@@ -111,6 +112,14 @@ class MultiDict(object):
     def __delitem__(self, key):
         """Delete all instances of the given key."""
         del self._dict[key]
+
+    def keys(self):
+        """Return a list of all present transition instances."""
+        warnings.warn("Method will return an iterator in future versions.", DeprecationWarning)
+        return [
+            key for key, (prop, mult) in self._dict.items()
+            for i in range(mult)
+        ]
 
     def items(self):
         """Iterate over key, value, multiplicity triples."""
@@ -168,6 +177,7 @@ class PriorityQueue(object):
     def __init__(self, value_callback):
         self._queue = pqdict()
         self.value_callback = value_callback
+        self.depletion_value = float('inf')
 
     def __bool__(self):
         return bool(self._queue)
@@ -176,6 +186,14 @@ class PriorityQueue(object):
 
     def __getitem__(self, key):
         return self._queue[key]
+
+    def keys(self):
+        """Return a list of all present transition instances."""
+        warnings.warn("Method will return an iterator in future versions.", DeprecationWarning)
+        return [
+            key for key, instances in self._queue.items()
+            for i in instances
+        ]
 
     def topitem(self):
         """Yield (value, key, (data,)) triple with lowest value."""
@@ -202,8 +220,8 @@ class PriorityQueue(object):
 
     def remove_item(self, key):
         """Remove all instances of key with a value of float('inf')"""
-        # XXX the value corresponding to depletion could be more generic
-        remaining = [t for t in self._queue[key] if t != float('inf')]
+        remaining = [t for t in self._queue[key]
+                     if t != self.depletion_value]
         if remaining:
             self._queue[key] = remaining
         else:
@@ -317,6 +335,16 @@ class TrajectorySampler(with_metaclass(abc.ABCMeta, object)):
         probability distributions.
         """
         return float('inf'), None, tuple()
+
+    @abc.abstractproperty
+    def transitions(self):
+        """Return list of all transitions
+
+        Implementations need to return every instance of an added
+        transition, i.e. two copies of a transition that has
+        multiplicity two.
+        """
+        return []
 
     def is_applicable(self, time, transition, *args):
         """True if the transition is applicable
@@ -434,6 +462,10 @@ class DirectMethod(TrajectorySampler):
             self.dependency_graph.remove_reaction(trans)
             del self.propensities[trans]
         self.depleted = []
+
+    @property
+    def transitions(self):
+        return self.propensities.keys()
 
     def propose_potential_transition(self):
         from math import log
@@ -566,6 +598,10 @@ class NextReactionMethod(FirstReactionMethod):
             self.dependency_graph.remove_reaction(trans)
             self.firings.remove_item(trans)
         self.depleted = []
+
+    @property
+    def transitions(self):
+        return self.firings.keys()
 
     def propose_potential_transition(self):
         if self.firings:

TODO
@@ -1,4 +1,3 @@
-? how to access TrajectorySampler.transitions?
 = feature/nrm
 * stocal/examples/typed_rules.py does not do what it claims to do
 * specify and test whether events at tmax are included in the trajectory or not

stocal/tests/test_algorithms.py
@@ -172,6 +172,14 @@ class TestTrajectorySampler(AbstractTestCase('Sampler', stocal.algorithms.Trajec
         self.assertEqual(sampler.step, 0)
         self.assertEqual(sampler.time, 0.)
 
+    def test_transitions_counts_mutliplicities(self):
+        """Sampler.transitions should give access to all transitions."""
+        proc = stocal.Process()
+        sampler = self.Sampler(proc, {})
+        sampler.add_transition(stocal.MassAction({}, {'a':1}, 1.))
+        sampler.add_transition(stocal.MassAction({}, {'a':1}, 1.))
+        sampler.add_transition(stocal.MassAction({}, {'b':1}, 1.))
+        self.assertEqual(len(sampler.transitions), 3)
 
 
 class TestDirectMethod(TestTrajectorySampler):

stocal/tests/test_tutorial.py
@@ -107,7 +107,6 @@ class TestTutorial(unittest.TestCase):
         for _ in trajectory:
             pass
 
-    @unittest.skip("Tests an implementation detail.")
     def test_types(self):
         """Specifying types via ReactionRule.signature"""
         process = Process(rules=[Association()])

commit 9d2337326772f509fb32118d3a1cf88e5585de87

Fix bug in FirstReactionMethod
class did not provide transition property. This bug was introduced
in commit 9e1778c91236383ac...

stocal/algorithms.py
@@ -521,12 +521,16 @@ class FirstReactionMethod(TrajectorySampler):
     See help(TrajectorySampler) for usage information.
     """
     def __init__(self, process, state, t=0., tmax=float('inf'), steps=None, seed=None):
-        self.transitions = []
+        self._transitions = []
         self.firings = []
         super(FirstReactionMethod, self).__init__(process, state, t, tmax, steps, seed)
 
+    @property
+    def transitions(self):
+        return self._transitions
+
     def add_transition(self, transition):
-        self.transitions.append(transition)
+        self._transitions.append(transition)
 
     def prune_transitions(self):
         depleted = [
@@ -534,13 +538,13 @@ class FirstReactionMethod(TrajectorySampler):
             if t == float('inf') and (r.rule or isinstance(r, Event))
         ]
         for i in reversed(depleted):
-            del self.transitions[i]
+            del self._transitions[i]
             del self.firings[i]
 
     def propose_potential_transition(self):
         self.firings = [
             (trans.next_occurrence(self.time, self.state, self.rng), trans, tuple())
-            for trans in self.transitions
+            for trans in self._transitions
         ]
 
         if self.firings: